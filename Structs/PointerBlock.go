5353f32a18fd701dbe84d7fa37f9d0ac1bd97c91
sudokentucky <3116549240704@ingenieria.usac.edu.gt>
2024-08-30 00:10:13 -0600

    Estructura PointerBlock para guardar los bloques de apuntadores

---
+++ Structs/PointerBlock.go
@@ -0,0 +1,6 @@
+package structs
+
+// BloqueApuntadores : Estructura para guardar los bloques de apuntadores
+type PointerBlock struct {
+	b_pointers [16]int64 //Apuntadores a bloques de carpetas
+}

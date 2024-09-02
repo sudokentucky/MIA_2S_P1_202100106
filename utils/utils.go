f7a918c0312d03b9a244860b8dabfb5e10368672
sudokentucky <3116549240704@ingenieria.usac.edu.gt>
2024-09-02 16:22:42 -0600

    Refactorizar función CreateParentDirs para mejorar legibilidad y manejo de errores

--- utils/utils.go
+++ utils/utils.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 // ConvertToBytes convierte un tamaño y una unidad a bytes
@@ -78,4 +80,24 @@ func WriteToFile(file *os.File, offset int64, data interface{}) error {
 	}
 
 	return nil
+}
+
+// createParentDirs crea las carpetas padre si no existen
+func CreateParentDirs(path string) error {
+	dir := filepath.Dir(path)
+	// os.MkdirAll no sobrescribe las carpetas existentes, solo crea las que no existen
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error al crear las carpetas padre: %v", err)
+	}
+	return nil
+}
+
+// getFileNames obtiene el nombre del archivo .dot y el nombre de la imagen de salida
+func GetFileNames(path string) (string, string) {
+	dir := filepath.Dir(path)
+	baseName := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+	dotFileName := filepath.Join(dir, baseName+".dot")
+	outputImage := path
+	return dotFileName, outputImage
 }

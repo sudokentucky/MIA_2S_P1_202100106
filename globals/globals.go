1041af1a77b109266955045433d8394f6024c547
sudokentucky <3116549240704@ingenieria.usac.edu.gt>
2024-09-02 16:22:09 -0600

    Refactorizar función GetMountedPartitionRep para mejorar legibilidad y manejo de errores

--- globals/globals.go
+++ globals/globals.go
@@ -44,3 +44,45 @@ func GetMountedPartition(id string) (*structures.Partition, string, error) {
 
 	return partition, path, nil
 }
+
+// GetMountedPartitionRep obtiene el MBR y el SuperBlock de la partición montada con el id especificado
+func GetMountedPartitionRep(id string) (*structures.MBR, *structures.Superblock, string, error) {
+	// Obtener el path de la partición montada
+	path := MountedPartitions[id]
+	if path == "" {
+		return nil, nil, "", errors.New("la partición no está montada")
+	}
+
+	// Abrir el archivo para leer el MBR
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, nil, "", err
+	}
+	defer file.Close()
+
+	// Crear una instancia de MBR
+	var mbr structures.MBR
+
+	// Deserializar la estructura MBR desde el archivo
+	err = mbr.Decode(file)
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	// Buscar la partición con el id especificado
+	partition, err := mbr.GetPartitionByID(id)
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	// Crear una instancia de SuperBlock
+	var sb structures.Superblock
+
+	// Deserializar la estructura SuperBlock desde el archivo
+	err = sb.Decode(file, int64(partition.Part_start))
+	if err != nil {
+		return nil, nil, "", err
+	}
+
+	return &mbr, &sb, path, nil
+}

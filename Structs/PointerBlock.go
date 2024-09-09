24584ccadd476b47642a2269440110970cc7c47f
sudokentucky <3116549240704@ingenieria.usac.edu.gt>
2024-09-09 16:29:38 -0600

    Archivos cambiados de directorio.

--- Structs/PointerBlock.go
+++
@@ -1,6 +0,0 @@
-package structs
-
-// BloqueApuntadores : Estructura para guardar los bloques de apuntadores
-type PointerBlock struct {
-	b_pointers [16]int64 //Apuntadores a bloques de carpetas
-}

7394648df54c1f82dafeb4d7c133c4e7d70f0a51
sudokentucky <3116549240704@ingenieria.usac.edu.gt>
2024-09-14 23:17:18 -0600

    Actualizar estructura FileBlock y utilizar funciones de utils

--- BackEnd/Structs/FileBlock.go
+++ BackEnd/Structs/FileBlock.go
@@ -1,28 +1,37 @@
 package structs
 
 import (
+	"backend/utils" // Asegúrate de ajustar el path del package "utils"
 	"fmt"
 	"os"
-
-	utilidades "backend/utils" // Importa el paquete utils
 )
 
-// FileBlock representa un bloque de archivo en el sistema de archivos
 type FileBlock struct {
 	B_content [64]byte
+	// Total: 64 bytes
 }
 
-// Encode serializa la estructura FileBlock en un archivo en la posición especificada
+// Encode serializa la estructura FileBlock en un archivo binario en la posición especificada
 func (fb *FileBlock) Encode(file *os.File, offset int64) error {
-	return utilidades.WriteToFile(file, offset, fb)
+	// Utilizamos la función WriteToFile del paquete utils
+	err := utils.WriteToFile(file, offset, fb)
+	if err != nil {
+		return fmt.Errorf("error writing FileBlock to file: %w", err)
+	}
+	return nil
 }
 
-// Decode deserializa la estructura FileBlock desde un archivo en la posición especificada
+// Decode deserializa la estructura FileBlock desde un archivo binario en la posición especificada
 func (fb *FileBlock) Decode(file *os.File, offset int64) error {
-	return utilidades.ReadFromFile(file, offset, fb)
+	// Utilizamos la función ReadFromFile del paquete utils
+	err := utils.ReadFromFile(file, offset, fb)
+	if err != nil {
+		return fmt.Errorf("error reading FileBlock from file: %w", err)
+	}
+	return nil
 }
 
-// Print prints the content of B_content as a string
+// PrintContent imprime el contenido de B_content como una cadena
 func (fb *FileBlock) Print() {
 	fmt.Printf("%s", fb.B_content)
 }
